a680991aace4229d5147f4614f13e71112d7f7ba
Bluelabmeme <113112292+bluenerdmeme@users.noreply.github.com>
2026-06-11 19:43:54 +0700

    Updated new domain/website
    Added vcc-librart.uk

---
+++ exo-api blocker.bat
@@ -0,0 +1,27 @@
+@echo off
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Requesting admin privileges...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
+set HOSTS=%SystemRoot%\System32\drivers\etc\hosts
+
+echo. >> "%HOSTS%"
+echo 127.0.0.1 exo-api.tf >> "%HOSTS%"
+echo 127.0.0.1 www.exo-api.tf >> "%HOSTS%"
+echo ::1 exo-api.tf >> "%HOSTS%"
+echo ::1 www.exo-api.tf >> "%HOSTS%"
+
+echo 127.0.0.1 https://vcc-library.uk/ >> "%HOSTS%"
+echo 127.0.0.1 https://vcc-library.uk/ >> "%HOSTS%"
+echo ::1 https://vcc-library.uk/ >> "%HOSTS%"
+echo ::1 https://vcc-library.uk/ >> "%HOSTS%"
+
+
+ipconfig /flushdns
+
+echo EXO-API.TF NOW BLOCKED
+echo vcc-librart.uk NOW BLOCKED
+pause
